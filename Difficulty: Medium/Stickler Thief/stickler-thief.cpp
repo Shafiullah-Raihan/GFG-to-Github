7fee4bf08f02eac34353ff405d165f3149bacc37
Shafiullah Raihan <140353900+Shafiullah-Raihan@users.noreply.github.com>
2025-12-12 03:13:13 +0600

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stickler Thief/stickler-thief.cpp
@@ -0,0 +1,18 @@
+
+class Solution {
+  public:
+    // Function to find the maximum money the thief can get.
+    int findMaxSum(vector<int>& arr) {
+        // Your code here
+        int n= arr.size();
+        vector<int>dp(n,0);
+        dp[0] = arr[0], dp[1] = max(arr[0],arr[1]);
+        for(int i=2;i<n;i++)
+        {
+            dp[i] = max(arr[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[n-1];
+        
+    }
+};
+
