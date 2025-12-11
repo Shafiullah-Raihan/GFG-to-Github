0ed697f26c6394d248219fcf430e014834b89113
Shafiullah Raihan <140353900+Shafiullah-Raihan@users.noreply.github.com>
2025-12-12 00:38:54 +0600

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
@@ -0,0 +1,32 @@
+
+class Solution {
+  public:
+    bool isBalanced(string& s) {
+       stack<char> st;
+    for (int i = 0; i < s.length(); i++) {
+        
+        // Check if the character is an opening bracket
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[') {
+            st.push(s[i]); 
+        } 
+      
+        else {
+
+ 
+            if (!st.empty() && 
+                ((st.top() == '(' && s[i] == ')') ||
+                 (st.top() == '{' && s[i] == '}') ||
+                 (st.top() == '[' && s[i] == ']'))) {
+
+                st.pop(); 
+            }
+            else {
+                
+                return false; 
+            }
+        }
+    }
+    
+    return st.empty();
+    }
+};
