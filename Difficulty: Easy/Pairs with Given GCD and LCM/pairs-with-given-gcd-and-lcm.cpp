33315717dad400f235eb0c27417324be08c2b364
Shafiullah Raihan <140353900+Shafiullah-Raihan@users.noreply.github.com>
2026-09-10 23:11:40 +0600

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pairs with Given GCD and LCM/pairs-with-given-gcd-and-lcm.cpp
@@ -0,0 +1,30 @@
+class Solution {
+  public:
+    int pairCount(int x, int y) {
+        // code here
+        int n = 0, res = 0;
+
+            // lcm must be divisible by gcd, else no pair exists
+            if (y % x == 0)
+                n = y / x;
+
+            // if n is 1, the only pair is (x, x)
+            if (n == 1)
+                res = 1;
+
+            // find coprime factor pairs (i, n/i) of n
+            for (int i = 1; i <= sqrt(n); i++) {
+                if (n % i == 0) {
+                    int j = n / i;
+
+                    // both (x*i, x*j) and (x*j, x*i) are valid
+                    // only if i and j are coprime (gcd = 1)
+                    if (i != j && __gcd(x * i, x * j) == x)
+                        res += 2;
+                }
+            }
+
+        return res;
+        
+    }
+};
